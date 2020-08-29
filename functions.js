0b2f3ef3743b8df01567b848e3dfa3cd65e38b4d
Giovanni Ranzato <giovanni.ranzato2010@gmail.com>
2020-08-30 01:29:55 +0200

    Adjust animations

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 let slideIndex = 0;
-let rotation = false;
-let first = true
+let flip = false;
+
 const data = [{
     name: "Tanya Sinclair",
     role: "UX Engineer",
@@ -12,32 +12,47 @@ const data = [{
     role: "Junior Front-end Developer",
     message: "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
     image: "./images/image-john.jpg"
-}];
+},
+{
+    name: "Tony Ledger",
+    role: "Software Engineer",
+    message: "“ I\’ve been interested in coding for a while but never taken the jump, until now. I couldn\’t recommend this course enough. I\’m now in the job of my dreams and so excited about the future. ”",
+    image: "./images/image-tony.jpg"
+},
+{
+    name: "Jane Rainolds",
+    role: "Back-end Developer",
+    message: "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+    image: "./images/image-jane.jpg"
+},
+];
 showSlide(slideIndex);
 
 function showSlide(index){
     document.getElementById("message").setAttribute('class', 'fadeOut');
+    document.getElementById("slideController").setAttribute('class', 'fadeOut');
     setTimeout(function () {
         document.getElementById("messageText").innerHTML = data[index].message;
         document.getElementById("messageName").innerHTML = data[index].name + "<span>" + data[index].role + "</span>";
         document.getElementById("message").setAttribute('class', 'fadeIn');
+        document.getElementById("slideController").setAttribute('class', 'fadeIn');
     }, 500);
-    if(rotation){
-        document.getElementById("visual").setAttribute('class', 'rotateY90');
-        setTimeout(function(){ 
-            document.getElementById("visualImg").setAttribute('src', data[index].image); 
-            document.getElementById("visual").setAttribute('class', 'rotateY180');
-            rotation = !rotation;
-         }, 500);
-         
+    if(flip){
+        if(index >= data.length){
+            document.getElementById("visualImgBack").setAttribute('src', data[0].image);
+        }else{
+            document.getElementById("visualImgBack").setAttribute('src', data[index].image);
+        }
+        document.getElementById("visual").setAttribute('class', 'flip');
     }else{
-        document.getElementById("visual").setAttribute('class', 'rotateY90');
-        setTimeout(function(){ 
-            document.getElementById("visualImg").setAttribute('src', data[index].image); 
-            document.getElementById("visual").setAttribute('class', 'rotateY0');
-            rotation = !rotation;
-         }, 500);
-    } 
+        document.getElementById("visual").setAttribute('class', 'unflip');
+        if(index >= data.length){
+            document.getElementById("visualImgFront").setAttribute('src', data[0].image);
+        }else{
+            document.getElementById("visualImgFront").setAttribute('src', data[index].image);
+        }
+    }
+    flip = !flip;
 }
 function switchSlide (flow){
     slideIndex=slideIndex+flow;
